ace2491683a659f4dfd945243f1b7e66a6e7f5f5
Vitaly Pikulik <vitaly.pikulik@deliveryhero.com>
2015-06-12 14:27:13 +0200

    Add field modified to examples and doc

--- examples/reader.sql
+++ examples/reader.sql
@@ -17,6 +17,7 @@ CREATE FOREIGN TABLE osm_malta (
     members json,
 
     version int,
+    modified timestamp,
     changeset bigint,
     user_id int,
     username text,
@@ -36,6 +37,7 @@ CREATE FOREIGN TABLE osm_monaco (
     members json,
 
     version int,
+    modified timestamp,
     changeset bigint,
     user_id int,
     username text,
@@ -55,6 +57,7 @@ CREATE FOREIGN TABLE osm_belarus (
     members json,
 
     version int,
+    modified timestamp,
     changeset bigint,
     user_id int,
     username text,
